b631fb8c69a280d970b579d7034ceb822a7b5522
buguno <brunnodesouzabezerra@gmail.com>
2025-03-18 10:31:17 -0300

    test(test_app): adding fixture

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from app.app import app
-
-client = TestClient(app)
-
-
-def test_root_return_ok_and_hello_world():
+def test_root_return_ok_and_hello_world(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
