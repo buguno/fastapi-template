94bf1f2b5e7f3e0c101b8b23625323d60da05851
buguno <brunnodesouzabezerra@gmail.com>
2025-03-18 23:13:18 -0300

    test(test_db): adding mock db time and assert user object

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+
 from faker import Faker
 from sqlalchemy import select
 
@@ -6,15 +8,22 @@ from app.models import User
 faker = Faker()
 
 
-def test_create_user(session):
+def test_create_user(session, mock_db_time):
     username = faker.user_name()
     password = faker.password()
     email = faker.email()
 
-    new_user = User(username=username, password=password, email=email)
-    session.add(new_user)
-    session.commit()
+    with mock_db_time(model=User) as time:
+        new_user = User(username=username, password=password, email=email)
+        session.add(new_user)
+        session.commit()
 
     user = session.scalar(select(User).where(User.username == username))
 
-    assert user.username == username
+    assert asdict(user) == {
+        'id': 1,
+        'username': username,
+        'password': password,
+        'email': email,
+        'created_at': time,
+    }
