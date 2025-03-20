17cc071c51629e2073065c26734143e17edad710
buguno <brunnodesouzabezerra@gmail.com>
2025-03-19 22:54:52 -0300

    feat(schemas): adding user schemas

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,19 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: str
+
+
+class UserDB(UserSchema):
+    id: int
