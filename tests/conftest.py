1b8f62f640e0225be4ac6965aa5237e979f461fd
buguno <brunnodesouzabezerra@gmail.com>
2025-03-19 23:06:53 -0300

    test(conftest): adding thread and static pool config in session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,21 +4,33 @@ from datetime import datetime
 import pytest
 from faker import Faker
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine, event
+from sqlalchemy import StaticPool, create_engine, event
 from sqlalchemy.orm import Session
 
 from app.app import app
+from app.database import get_session
 from app.models import table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture
 def session():
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
