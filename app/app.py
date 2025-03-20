1987c47f7dcdd5a11d73ac670a9fb411da729b87
buguno <brunnodesouzabezerra@gmail.com>
2025-03-19 22:55:19 -0300

    feat(app): adding create user endpoint

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from app.schemas import Message
+from app.database import get_session
+from app.models import User
+from app.schemas import Message, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -10,3 +14,30 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Hello World!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT, detail='Email already exists'
+            )
+
+    db_user = User(**user.model_dump())
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
