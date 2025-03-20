e5c3328e471a78eba95d89ad875e3c380b57b421
buguno <brunnodesouzabezerra@gmail.com>
2025-03-19 23:07:12 -0300

    test(test_app): create user

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,3 +6,25 @@ def test_root_return_ok_and_hello_world(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World!'}
+
+
+def test_create_user(client, faker):
+    username = faker.user_name()
+    email = faker.email()
+    password = faker.password()
+
+    response = client.post(
+        '/users',
+        json={
+            'username': username,
+            'email': email,
+            'password': password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': username,
+        'email': email,
+    }
