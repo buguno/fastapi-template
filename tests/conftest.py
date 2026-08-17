a7ac6ec571d7f13fc3a5b2ca61fff6a51e3447f2
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 15:22:53 -0300

    test(conftest): fixing auth token router

--- tests/conftest.py
+++ tests/conftest.py
@@ -83,7 +83,7 @@ def user(session, faker):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        '/token',
+        '/auth/token',
         data={'username': user.email, 'password': user.clean_password},
     )
     return response.json()['access_token']
