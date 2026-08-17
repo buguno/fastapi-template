943e5a4e656930b8f3f5a3f3c0e83eda7684b0c6
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 15:21:09 -0300

    feat(adding annotated in sessions and token and current user

--- app/security.py
+++ app/security.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from http import HTTPStatus
+from typing import Annotated
 from zoneinfo import ZoneInfo
 
 from fastapi import Depends, HTTPException
@@ -7,14 +8,13 @@ from fastapi.security import OAuth2PasswordBearer
 from jwt import DecodeError, decode, encode
 from pwdlib import PasswordHash
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
-from app.database import get_session
+from app.database import DbSession
 from app.models.user import User
 from app.settings import settings
 
 pwd_context = PasswordHash.recommended()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 def get_password_hash(password: str):
@@ -38,8 +38,8 @@ def create_access_token(data: dict):
 
 
 def get_current_user(
-    session: Session = Depends(get_session),
-    token: str = Depends(oauth2_scheme),
+    session: DbSession,
+    token: Annotated[str, Depends(oauth2_scheme)],
 ):
     credentials_exception = HTTPException(
         status_code=HTTPStatus.UNAUTHORIZED,
@@ -65,3 +65,6 @@ def get_current_user(
         raise credentials_exception
 
     return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
