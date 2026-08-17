867f123ed120e7435dbf80db24578f998f4aeb30
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 16:16:52 -0300

    test(security): creating token without and unknown subject tests

--- tests/test_security.py
+++ tests/test_security.py
@@ -23,3 +23,25 @@ def test_jwt_invalid_token(client):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_jwt_token_without_subject(client):
+    token = create_access_token({'test': 'test'})
+
+    response = client.delete(
+        '/users/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_jwt_token_with_unknown_subject(client, faker):
+    token = create_access_token({'sub': faker.email()})
+
+    response = client.delete(
+        '/users/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
