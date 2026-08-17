62009de3f447f0e7fdedb89af8077f377e9bc24a
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 20:15:15 -0300

    test(auth): creating tests using freeze time to test expired tokens

--- tests/routers/test_auth.py
+++ tests/routers/test_auth.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
+
 
 def test_get_token(client, user):
     response = client.post(
@@ -31,3 +33,38 @@ def test_get_token_with_wrong_password(client, user, faker):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_refresh_token(client, token):
+    response = client.post(
+        '/auth/refresh',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert 'access_token' in response.json()
+    assert response.json()['token_type'] == 'bearer'
+
+
+def test_refresh_token_without_credentials(client):
+    response = client.post('/auth/refresh')
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+
+
+def test_refresh_token_expired_does_not_renew(client, user):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = client.post(
+            '/auth/refresh',
+            headers={'Authorization': f'Bearer {token}'},
+        )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
