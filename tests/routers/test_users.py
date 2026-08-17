7acab4172a5dca84af073d14701151f3510f49e0
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 16:27:12 -0300

    test(users): creating users crud routers

---
+++ tests/routers/test_users.py
@@ -0,0 +1,91 @@
+from http import HTTPStatus
+
+from app.schemas.user import UserPublic
+
+
+def test_create_user(client, faker):
+    username = faker.user_name()
+    password = faker.password()
+    email = faker.email()
+
+    response = client.post(
+        '/users',
+        json={
+            'username': username,
+            'email': email,
+            'password': password,
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': username,
+        'email': email,
+        'id': 1,
+    }
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user, token, faker):
+    username = faker.user_name()
+    email = faker.email()
+    password = faker.password()
+
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': username,
+            'email': email,
+            'password': password,
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': username,
+        'email': email,
+        'id': user.id,
+    }
+
+
+def test_update_integrity_error(client, user, token, faker):
+    username = faker.user_name()
+    email = faker.email()
+    password = faker.password()
+
+    client.post(
+        '/users',
+        json={
+            'username': username,
+            'email': email,
+            'password': password,
+        },
+    )
+
+    response_update = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': username,
+            'email': user.email,
+            'password': password,
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email already exists'
+    }
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
