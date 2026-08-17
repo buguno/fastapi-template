6bfac0b1f66e83e84f30fbb4d49ddc8336003d90
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 17:56:03 -0300

    test(database): changing sync to async

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,15 +1,16 @@
 import pytest
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_session
 
 
-def test_get_session_yields_and_closes_a_session():
+@pytest.mark.asyncio
+async def test_get_session_yields_and_closes_an_async_session():
     session_generator = get_session()
-    session = next(session_generator)
+    session = await anext(session_generator)
 
-    assert isinstance(session, Session)
+    assert isinstance(session, AsyncSession)
     assert session.is_active
 
-    with pytest.raises(StopIteration):
-        next(session_generator)
+    with pytest.raises(StopAsyncIteration):
+        await anext(session_generator)
