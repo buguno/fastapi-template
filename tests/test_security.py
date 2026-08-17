0b435d205eebafac26dc8fa32690f42dbb6a45a7
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 20:15:36 -0300

    test(security): adding token test with freeze time

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
 from jwt import decode
 
 from app.security import create_access_token
@@ -45,3 +46,27 @@ def test_jwt_token_with_unknown_subject(client, faker):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_expired_after_time(client, user, faker):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = client.put(
+            f'/users/{user.id}',
+            headers={'Authorization': f'Bearer {token}'},
+            json={
+                'username': faker.user_name(),
+                'email': faker.email(),
+                'password': faker.password(),
+            },
+        )
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
