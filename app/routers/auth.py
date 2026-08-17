5501d21842aa6bef3e98b89554d25fc019ecd11e
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 15:18:36 -0300

    feat(auth): creating auth token router

---
+++ app/routers/auth.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.database import DbSession
+from app.schemas.token import Token
+from app.services import auth as auth_service
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(form_data: OAuth2Form, session: DbSession):
+    access_token = auth_service.authenticate(
+        session, form_data.username, form_data.password
+    )
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
