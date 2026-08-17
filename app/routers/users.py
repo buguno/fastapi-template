d0b3bb18613fc2d76b993b1a20c9fadd50debda5
buguno <brunnodesouzabezerra@gmail.com>
2026-08-17 17:46:40 -0300

    refactor(user): changing user routers to async

--- app/routers/users.py
+++ app/routers/users.py
@@ -14,35 +14,37 @@ router = APIRouter(prefix='/users', tags=['users'])
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(data: UserSchema, session: DbSession):
-    new_user = user_service.create_user(session, data)
+async def create_user(data: UserSchema, session: DbSession):
+    new_user = await user_service.create_user(session, data)
     return new_user
 
 
 @router.get('/', response_model=UserList)
-def read_users(session: DbSession, filters: Annotated[FilterPage, Query()]):
-    users = user_service.list_users(session, filters)
+async def read_users(
+    session: DbSession, filters: Annotated[FilterPage, Query()]
+):
+    users = await user_service.list_users(session, filters)
     return {'users': users}
 
 
 @router.put('/{user_id}', response_model=UserPublic)
-def update_user(
+async def update_user(
     user_id: int,
     data: UserSchema,
     session: DbSession,
     current_user: CurrentUser,
 ):
-    updated_user = user_service.update_user(
+    updated_user = await user_service.update_user(
         session, user_id, data, current_user
     )
     return updated_user
 
 
 @router.delete('/{user_id}', response_model=Message)
-def delete_user(
+async def delete_user(
     user_id: int,
     session: DbSession,
     current_user: CurrentUser,
 ):
-    user_service.delete_user(session, user_id, current_user)
+    await user_service.delete_user(session, user_id, current_user)
     return {'message': 'User deleted'}
